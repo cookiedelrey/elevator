3e16ee75f9b0cb7a202caab7bb95d76a684d557f
cookiedelrey <aikanysh.akynbekova.t@gmail.com~>
2023-01-20 18:19:03 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+let elevator = document.querySelector(".elevator");
+let box = document.querySelector('.elevator')
+
+const btn1 = document.querySelector(".one");
+btn1.addEventListener("click", ()=>{
+    box.style.top = '1125px'
+})
+const btn2 = document.querySelector(".two");
+btn2.addEventListener("click", ()=>{
+    box.style.top = '1050px'
+})
+const btn3 = document.querySelector(".three");
+btn3.addEventListener("click", ()=>{
+    box.style.top = '975px'
+})
+const btn4 = document.querySelector(".four");
+btn4.addEventListener("click", ()=>{
+    box.style.top = '905px'
+})
+const btn5 = document.querySelector(".five");
+btn5.addEventListener("click", ()=>{
+    box.style.top = '825px'
+})
+const btn6 = document.querySelector(".six");
+btn6.addEventListener("click", ()=>{
+    box.style.top = '750px'
+})
+const btn7 = document.querySelector(".seven");
+btn7.addEventListener("click", ()=>{
+    box.style.top = '675px'
+})
+const btn8 = document.querySelector(".eight");
+btn8.addEventListener("click", ()=>{
+    box.style.top = '600px'
+})
+const btn9 = document.querySelector(".nine");
+btn9.addEventListener("click", ()=>{
+    box.style.top = '525px'
+})
+const btn10 = document.querySelector(".ten");
+btn10.addEventListener("click", ()=>{
+    box.style.top = '455px'
+})
+const btn11 = document.querySelector(".eleven");
+btn11.addEventListener("click", ()=>{
+    box.style.top = '375px'
+})
+const btn12 = document.querySelector(".twelve");
+btn12.addEventListener("click", ()=>{
+    box.style.top = '300px'
+})
+const btn13 = document.querySelector(".thirt");
+btn13.addEventListener("click", ()=>{
+    box.style.top = '225px'
+})
+const btn14 = document.querySelector(".fourt");
+btn14.addEventListener("click", ()=>{
+    box.style.top = '155px'
+})
+const btn15 = document.querySelector(".fift");
+btn15.addEventListener("click", ()=>{
+    box.style.top = '75px'
+})
+const btn16 = document.querySelector(".sixt");
+btn16.addEventListener("click", ()=>{
+    box.style.top = '5px'
+})
